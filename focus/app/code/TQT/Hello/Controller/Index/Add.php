4bbc4085d7571ff52f6e0aede7de8437763b97b1
QuangTrung96 <trinhquangtrung646@gmail.com>
2018-06-20 09:21:54 +0700

    [create_a_object_block_and_run_registry]

---
+++ focus/app/code/TQT/Hello/Controller/Index/Add.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TQT\Hello\Controller\Index;
+
+class Add extends Index
+{
+	public function execute() {
+		parent::execute();
+	}
+
+	
+}
---
+++ focus/app/code/TQT/Hello/Controller/Index/Delete.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TQT\Hello\Controller\Index;
+
+class Delete extends Index
+{
+	public function execute() {
+		parent::execute();
+	}
+	
+}
